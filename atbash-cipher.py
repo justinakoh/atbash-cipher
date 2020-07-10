e11644dc1be501261375563187431f94556d3ec7
justinakoh <justina.y.koh@gmail.com>
2020-07-10 12:44:43 +1200

    Update atbash code to handle non letters
    This code appears to work when tested manually.
    Next step is to write tests to check if the code has been properly implemented

--- atbash-cipher.py
+++ atbash-cipher.py
@@ -1,6 +1,5 @@
 from string import ascii_uppercase as l
 
-
 def main():
     start_message = input(
         "This is an encoder/decoder which uses the AtBash Cipher. Do you wish to continue? "
@@ -9,12 +8,16 @@ def main():
         # Create table with the alphabet reversed
         reversed_list = list(createTable())
 
-        # Ask for input
-        plaintext = input("Please enter your plaintext ")
+        # Ask for input: convert it to lowercase
+        plaintext = input("Please enter your plaintext ").lower()
 
         encoded_text = ""
         for i in plaintext:
-            encoded_text += reversed_list[ord(i) - 97]
+            if i.isalpha():
+                encoded_text += reversed_list[ord(i) - 97]
+            # Don't encode non letters
+            else:
+                encoded_text += i
 
         # Output it
         print("The encoded text is: ", encoded_text)
@@ -24,6 +27,7 @@ def main():
 # This reverses the alphabet refer to: https://stackoverflow.com/questions/509211/understanding-slice-notation for more information on slicing
 def createTable():
     return str(l[::-1])
+
 
 if __name__ == "__main__":
     main()
